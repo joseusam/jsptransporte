bbb5a6a6cdd458b2ab5d2808579563f75ae9c488
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-13 15:30:25 -0600

    Apoderado

---
+++ transporte/src/main/java/com/empresa/dao/ApoderadoDao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.empresa.dao;
+
+import com.empresa.conexion.Conexion;
+import com.empresa.model.ApoderadoBean;
+import com.empresa.model.PersonaBean;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+/**
+ *
+ * @author moises.alvarengausam
+ */
+public class ApoderadoDao {
+    private Conexion conn;
+    private ApoderadoBean apoBea;
+    private PersonaBean perBea;
+    private List<ApoderadoBean> apoLS;
+    
+    private ResultSet rs;
+    private PreparedStatement ps;
+    private String sql;
+
+    public ApoderadoDao(Conexion conn) {
+        this.conn = conn;
+    }
+    
+    
+}
