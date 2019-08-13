99df4444aa58a459b7df55cf429b3432639e09ee
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-13 14:35:57 -0600

    Prueba de subida

--- transporte/src/main/java/com/empresa/dao/ModalidadDao.java
+++ transporte/src/main/java/com/empresa/dao/ModalidadDao.java
@@ -9,6 +9,7 @@ import com.empresa.conexion.Conexion;
 import com.empresa.model.ModalidadBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,32 +17,73 @@ import java.util.List;
  * @author moises.alvarengausam
  */
 public class ModalidadDao {
+
     private Conexion conn;
     private ModalidadBean modBea;
     private List<ModalidadBean> modLS;
-    
+
     private ResultSet rs;
     private PreparedStatement ps;
     private String sql;
-    
-    public ModalidadDao(Conexion con){
+
+    public ModalidadDao(Conexion con) {
         this.conn = con;
     }
-    
-    public boolean eliminar(int id){
+
+    public boolean eliminar(int id) {
         sql = "DELETE FROM modalidad WHERE idmodalidad=?";
         try {
             ps = conn.conexion().prepareStatement(sql);
             ps.setInt(1, id);
             ps.executeUpdate();
-            
+
             return true;
         } catch (Exception e) {
+            System.out.println("Error en Eliminar: " + e);
+            return false;
         }
-        return false;
     }
-    
-    public List<ModalidadBean> buscarTodo(){
-        return null;
+
+    public List<ModalidadBean> buscarTodo() {
+        sql = "SELECT * FROM modalidad";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            modLS = new LinkedList<>();
+
+            while (rs.next()) {
+                modBea = new ModalidadBean(rs.getInt("idmodalidad"));
+                modBea.setDescri(rs.getString("descripcion"));
+
+                modLS.add(modBea);
+            }return modLS;
+
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: " + e);
+            return null;
+        }
+    }
+
+    public List<ModalidadBean> buscaPorId(int id) {
+        sql = "SELECT * FROM modalidad WHERE idmodalidad=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            modLS = new LinkedList<>();
+
+            while (rs.next()) {
+                modBea = new ModalidadBean(rs.getInt("idmodalidad"));
+                modBea.setDescri(rs.getString("descripcion"));
+
+                modLS.add(modBea);
+            }return modLS;
+            
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id");
+            return null;
+        }
     }
 }
