e82b6bfb490adae9948e0aff248d5aa4a2b7b142
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-13 15:38:49 -0600

    Apoderado + Method

--- transporte/src/main/java/com/empresa/dao/ApoderadoDao.java
+++ transporte/src/main/java/com/empresa/dao/ApoderadoDao.java
@@ -10,6 +10,7 @@ import com.empresa.model.ApoderadoBean;
 import com.empresa.model.PersonaBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -30,5 +31,62 @@ public class ApoderadoDao {
         this.conn = conn;
     }
     
+    public boolean eliminar(int id){
+        sql = "DELETE FROM apoderado WHERE idapoderado";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
     
+    public List<ApoderadoBean> buscarTodo(){
+        sql = "SELECT * FROM apoderado";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            apoLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                apoBea = new ApoderadoBean(rs.getInt("idapoderado"));
+                perBea = new PersonaBean(rs.getInt("idpersona"));
+                apoBea.setIdperson(perBea);
+                
+                apoLS.add(apoBea);
+            }return apoLS;
+            
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<ApoderadoBean> buscarById(int id){
+        sql = "SELECT * FROM apoderado WHERE idapoderado=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            apoLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                apoBea = new ApoderadoBean(rs.getInt("idapoderado"));
+                perBea = new PersonaBean(rs.getInt("idpersona"));
+                apoBea.setIdperson(perBea);
+                
+                apoLS.add(apoBea);
+            }return apoLS;
+            
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
 }
