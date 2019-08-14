8cd47370e6166c069429b9de9ef27cc6e7a1c86d
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:47:10 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/RolDao.java
+++ transporte/src/main/java/com/empresa/dao/RolDao.java
@@ -9,6 +9,7 @@ import com.empresa.conexion.Conexion;
 import com.empresa.model.RolBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -26,6 +27,67 @@ public class RolDao {
 
     public RolDao(Conexion conn) {
         this.conn = conn;
+    }
+    
+    public boolean eliminar(int id){
+        sql = "DELETE FROM rol WHERE idrol=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<RolBean> buscarTodo(){
+        sql = "SELECT * FROM rol";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            rolLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rolBea = new RolBean(rs.getInt("idrol"));
+                rolBea.setDescri(rs.getString("descripcion"));
+                
+                rolLS.add(rolBea);
+            }
+            
+            return rolLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<RolBean> buscarPorId(int id){
+        sql = "SELECT * FROM rol WHERE idrol=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            rolLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rolBea = new RolBean(rs.getInt("idrol"));
+                rolBea.setDescri(rs.getString("descripcion"));
+                
+                rolLS.add(rolBea);
+            }
+            
+            return rolLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
     }
     
 }
