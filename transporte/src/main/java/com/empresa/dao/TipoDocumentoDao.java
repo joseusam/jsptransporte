c55434e6e1f07621c90aeb9fc0226b8699ae2f80
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:46:55 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/TipoDocumentoDao.java
+++ transporte/src/main/java/com/empresa/dao/TipoDocumentoDao.java
@@ -9,6 +9,7 @@ import com.empresa.conexion.Conexion;
 import com.empresa.model.TipoDocumentoBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -28,4 +29,64 @@ public class TipoDocumentoDao {
         this.conn = conn;
     }
     
+    public boolean eliminar(int id){
+        sql = "DELETE FROM tipo_documento WHERE idtipo_documento=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<TipoDocumentoBean> buscarTodo(){
+        sql = "SELECT * FROM tipo_documento";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            tipDLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                tipDBea = new TipoDocumentoBean(rs.getInt("idtipo_documento"));
+                tipDBea.setDescri(rs.getString("descripcion"));
+                
+                tipDLS.add(tipDBea);
+            }
+            
+            return tipDLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<TipoDocumentoBean> buscarPorId(int id){
+        sql = "SELECT * FROM tipo_documento WHERE idtipo_documento=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            tipDLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                tipDBea = new TipoDocumentoBean(rs.getInt("idtipo_documento"));
+                tipDBea.setDescri(rs.getString("descripcion"));
+                
+                tipDLS.add(tipDBea);
+            }
+            
+            return tipDLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
+    }
 }
