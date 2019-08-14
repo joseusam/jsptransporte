1a363c334adfb7dafc4e00d8376c2164c3dbbfbb
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:46:36 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/RutaModalidadDao.java
+++ transporte/src/main/java/com/empresa/dao/RutaModalidadDao.java
@@ -12,6 +12,7 @@ import com.empresa.model.RutaModalidadBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -34,5 +35,75 @@ public class RutaModalidadDao {
     public RutaModalidadDao(Conexion conn) {
         this.conn = conn;
     }
+    
+    public boolean eliminar(int id){
+        sql = "DELETE FROM WHERE ruta_modalidad=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<RutaModalidadBean> buscarTodo(){
+        sql = "SELECT * FROM ruta_modalidad";
         
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            rutMLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rutMBea = new RutaModalidadBean(rs.getInt("idruta_modalidad"));
+                rutMBea.setHora(rs.getDate("hora"));
+                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                rutMBea.setIdruta(rutBea);
+                modBean = new ModalidadBean(rs.getInt("idmodalidad"));
+                rutMBea.setIdmodali(modBean);
+                
+                rutMLS.add(rutMBea);
+            }
+            
+            return rutMLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<RutaModalidadBean> buscarPorId(int id){
+        sql = "SELECT * FROM ruta_modalidad WHERE idruta_modalidad=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            rutMLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rutMBea = new RutaModalidadBean(rs.getInt("idruta_modalidad"));
+                rutMBea.setHora(rs.getDate("hora"));
+                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                rutMBea.setIdruta(rutBea);
+                modBean = new ModalidadBean(rs.getInt("idmodalidad"));
+                rutMBea.setIdmodali(modBean);
+                
+                rutMLS.add(rutMBea);
+            }
+            
+            return rutMLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
+    }        
 }
