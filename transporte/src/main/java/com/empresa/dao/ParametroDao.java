b4d48637a690ca19e16a9f467cf88bbf8ff541d6
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:43:29 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/ParametroDao.java
+++ transporte/src/main/java/com/empresa/dao/ParametroDao.java
@@ -9,6 +9,7 @@ import com.empresa.conexion.Conexion;
 import com.empresa.model.ParametroBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -27,5 +28,66 @@ public class ParametroDao {
     public ParametroDao(Conexion conn) {
         this.conn = conn;
     }
-       
+     
+    public boolean eliminar(int id){
+        sql = "DELETE FROM parametro WHERE idparametro=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<ParametroBean> buscarTodo(){
+        sql = "SELECT * FROM parametro";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            paramLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                paramBean = new ParametroBean(rs.getInt("idparametro"));
+                paramBean.setDescri(rs.getString("descripcion"));
+                paramBean.setValor(rs.getDouble("valor"));
+                
+                paramLS.add(paramBean);
+            }
+            
+            return paramLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<ParametroBean> buscarPorID(){
+        sql = "SELECT * FROM parametro WHERE idparametro=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            paramLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                paramBean = new ParametroBean(rs.getInt("idparametro"));
+                paramBean.setDescri(rs.getString("descripcion"));
+                paramBean.setValor(rs.getDouble("valor"));
+                
+                paramLS.add(paramBean);
+            }
+            
+            return paramLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
+    }
 }
