b2d81354480d6d4a6694f05e5569e660dbe4fa19
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:47:30 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/RutaDao.java
+++ transporte/src/main/java/com/empresa/dao/RutaDao.java
@@ -6,9 +6,11 @@
 package com.empresa.dao;
 
 import com.empresa.conexion.Conexion;
+import com.empresa.model.DireccionBean;
 import com.empresa.model.RutaBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -18,7 +20,7 @@ import java.util.List;
 public class RutaDao {
     private Conexion conn;
     private RutaBean rutBea;
-    private DireccionDao dirBea;
+    private DireccionBean dirBea;
     private List<RutaBean> rutaLS;
     
     private PreparedStatement ps;
@@ -27,6 +29,69 @@ public class RutaDao {
 
     public RutaDao(Conexion conn) {
         this.conn = conn;
+    }
+    
+    public boolean eliminar(int id){
+        sql = "DELETE FROM ruta WHERE idruta=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<RutaBean> buscarTodo(){
+        sql = "SELECT * FROM ruta";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            rutaLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                dirBea = new DireccionBean(rs.getInt("iddireccion"));
+                rutBea.setIdinicio(dirBea);
+                
+                rutaLS.add(rutBea);
+            }
+            
+            return rutaLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<RutaBean> buscarPorId(int id){
+        sql = "SELECT * FROM ruta WHERE idruta=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            rutaLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                dirBea = new DireccionBean(rs.getInt("iddireccion"));
+                rutBea.setIdinicio(dirBea);
+                
+                rutaLS.add(rutBea);
+            }
+            
+            return rutaLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
     }
     
 }
