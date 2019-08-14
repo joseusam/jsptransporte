abf1e53219b79904270527dedbff28c20b65342d
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:44:00 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/ParadaNinioDao.java
+++ transporte/src/main/java/com/empresa/dao/ParadaNinioDao.java
@@ -6,10 +6,12 @@
 package com.empresa.dao;
 
 import com.empresa.conexion.Conexion;
+import com.empresa.model.NinioBean;
 import com.empresa.model.ParadaBean;
 import com.empresa.model.ParadaNinioBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,8 +21,9 @@ import java.util.List;
 public class ParadaNinioDao {
     private Conexion conn;
     private ParadaNinioBean parNBea;
+    private NinioBean ninBea;
     private ParadaBean parB;
-    private List<ParadaBean> parBLS;
+    private List<ParadaNinioBean> parNLS;
     
     private PreparedStatement ps;
     private ResultSet rs;
@@ -30,4 +33,75 @@ public class ParadaNinioDao {
         this.conn = conn;
     }
     
+    public boolean eliminar(int id){
+        sql = "DELETE FROM parada_ninio WHERE idparada_ninio=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<ParadaNinioBean> buscarTodo(){
+        sql = "SELECT * FROM parada_ninio";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            parNLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                parNBea = new ParadaNinioBean(rs.getInt("idparada_ninio"));
+                
+                ninBea = new NinioBean(rs.getInt("idninio"));
+                parNBea.setIdninio(ninBea);
+                parB = new ParadaBean(rs.getInt("idparada"));
+                parNBea.setIdinicio(parB);
+                parB = new ParadaBean(rs.getInt("idparada"));
+                parNBea.setIdfinal(parB);
+                
+                parNLS.add(parNBea);
+            }
+            
+            return parNLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo: "+e);
+            return null;
+        }
+    }
+    
+    public List<ParadaNinioBean> buscarPorId(){
+        sql = "SELECT * FROM parada_ninio WHERE idparada_ninio=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            parNLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                parNBea = new ParadaNinioBean(rs.getInt("idparada_ninio"));
+                
+                ninBea = new NinioBean(rs.getInt("idninio"));
+                parNBea.setIdninio(ninBea);
+                parB = new ParadaBean(rs.getInt("idparada"));
+                parNBea.setIdinicio(parB);
+                parB = new ParadaBean(rs.getInt("idparada"));
+                parNBea.setIdfinal(parB);
+                
+                parNLS.add(parNBea);
+            }
+            
+            return parNLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id: "+e);
+            return null;
+        }
+    }
 }
