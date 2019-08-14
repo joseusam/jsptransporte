0fbeae4d556c3b6af0d078370f1cb7c4c637fcd2
moises.alvarengausam <moises.alvarengausam@CDS2-USAM026.sv.cds>
2019-08-14 14:44:09 -0600

    Variables Dao Final

--- transporte/src/main/java/com/empresa/dao/ParadaDao.java
+++ transporte/src/main/java/com/empresa/dao/ParadaDao.java
@@ -11,6 +11,7 @@ import com.empresa.model.ParadaBean;
 import com.empresa.model.RutaBean;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -32,5 +33,75 @@ public class ParadaDao {
         this.conn = conn;
     }
     
+    public boolean eliminar(int id){
+        sql = "DELETE FROM parada WHERE idparada=?";
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+            
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error en Eliminar: "+e);
+            return false;
+        }
+    }
+    
+    public List<ParadaBean> buscarTodo(){
+        sql = "SELECT * FROM parada";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            paraLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                parBea = new ParadaBean(rs.getInt("idparada"));
+                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                parBea.setIdruta(rutBea);
+                dirBea = new DireccionBean(rs.getInt("iddireccion"));
+                parBea.setIddirecc(dirBea);
+                parBea.setOrden(rs.getInt("orden"));
+                
+                paraLS.add(parBea);
+            }
+            
+            return paraLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Todo:"+e);
+            return null;
+        }
+    }
+    
+    public List<ParadaBean> buscarPorId(int id){
+        sql = "SELECT * FROM parada WHERE idparada=?";
+        
+        try {
+            ps = conn.conexion().prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            
+            paraLS = new LinkedList<>();
+            
+            while (rs.next()) {                
+                parBea = new ParadaBean(rs.getInt("idparada"));
+                
+                rutBea = new RutaBean(rs.getInt("idruta"));
+                parBea.setIdruta(rutBea);
+                dirBea = new DireccionBean(rs.getInt("iddireccion"));
+                parBea.setIddirecc(dirBea);
+                parBea.setOrden(rs.getInt("orden"));
+                
+                paraLS.add(parBea);
+            }
+            
+            return paraLS;
+        } catch (Exception e) {
+            System.out.println("Error en Buscar Por Id:"+e);
+            return null;
+        }
+    }
     
 }
